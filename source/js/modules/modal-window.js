37f76f5fd92c0663fb4bcb0ec99cbd8dfbf8971e
Anastasia <achernova1551@gmail.com>
2022-05-29 14:15:15 +0300

    исправления

--- source/js/modules/modal-window.js
+++ source/js/modules/modal-window.js
@@ -2,24 +2,48 @@ const feedbackModal = document.querySelector('[data-element="feedback"]');
 const phoneButton = document.querySelector('[data-element="call"]');
 const closeButton = document.querySelector('[data-element="button-close"]');
 const modalOverlay = document.getElementById('modal-overlay');
-const modaFocus = document.getElementById('modal-name');
+const modalFocus = document.getElementById('modal-name');
+const scrollBody = document.getElementById('body');
 
 const getModal = () => {
+  feedbackModal.focus();
   phoneButton.addEventListener('click', function () {
     if (feedbackModal.classList.contains('is-invalid')) {
       feedbackModal.classList.remove('is-invalid');
       feedbackModal.classList.add('is-open');
-      modaFocus.focus();
+      scrollBody.classList.add('scroll-hidden');
+      modalFocus.focus();
     }
     closeButton.addEventListener('click', function () {
       feedbackModal.classList.remove('is-open');
       feedbackModal.classList.add('is-invalid');
+      scrollBody.classList.remove('scroll-hidden');
     });
   }, {passive: true});
 
   modalOverlay.addEventListener('click', function () {
     feedbackModal.classList.add('is-invalid');
     feedbackModal.classList.remove('is-open');
+    scrollBody.classList.remove('scroll-hidden');
   }, {passive: true});
+
+  window.addEventListener('click', outsideClick);
+  function outsideClick(e) {
+    if (e.target === feedbackModal) {
+      feedbackModal.classList.add('is-invalid');
+      feedbackModal.classList.remove('is-open');
+      scrollBody.classList.remove('scroll-hidden');
+    }
+  }
+
+  window.addEventListener('keyup', function (e) {
+    let key = e.keyCode;
+    if (key === 27) {
+      feedbackModal.classList.add('is-invalid');
+      feedbackModal.classList.remove('is-open');
+      scrollBody.classList.remove('scroll-hidden');
+    }
+  }, {passive: true});
+
 };
 export {getModal};
